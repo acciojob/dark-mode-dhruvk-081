a758a2264cf0df234ab5fa8216095f39263e0340
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-07-01 13:24:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let body = document.getElementsByTagName("body")[0];
+let btn = document.querySelector("button");
+let heading = document.querySelector("h1");
+let darkModeEnabled = localStorage.getItem("darkModeEnabled");
+
+if (darkModeEnabled === null) {
+  darkModeEnabled = "false";
+  localStorage.setItem("darkModeEnabled", darkModeEnabled);
+}
+
+if (darkModeEnabled === "true") {
+  enableDarkMode();
+} else {
+  disableDarkMode();
+}
+
+btn.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+  if (darkModeEnabled === "true") {
+    darkModeEnabled = "false";
+    disableDarkMode();
+  } else {
+    darkModeEnabled = "true";
+    enableDarkMode();
+  }
+  localStorage.setItem("darkModeEnabled", darkModeEnabled);
+}
+
+function enableDarkMode() {
+  body.classList.add("darkMode");
+  heading.textContent = "Dark Mode";
+}
+
+function disableDarkMode() {
+  body.classList.remove("darkMode");
+  heading.textContent = "Light Mode";
+}
